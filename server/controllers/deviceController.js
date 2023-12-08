00c9c16447c808ac85cf08d355f176f24597ef41
Aleksei Chernyshev <chernyshev1998@gmail.com>
2023-12-08 18:56:09 +0500

    reset Id count

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -2,16 +2,18 @@ const uuid = require('uuid');
 const path = require('path');
 const {Device} = require('../models/models')
 const ApiError = require('../error/ApiError')
+const reset = require('./resetId')
 
 class DeviceController {
   async create(req, res, next){
     try {
-      const {name, price, drandId, typeId, info} = req.body
+      const {name, price, brandId, typeId, info} = req.body
       const {img} = req.files
       let fileName = uuid.v4() + '.jpg'
       img.mv(path.resolve(__dirname, '..', 'static', fileName))
 
-      const device = await Device.create({name, price, drandId, typeId, img: fileName})
+      reset.resetId('devices')
+      const device = await Device.create({name, price, brandId, typeId, img: fileName})
 
       return res.json(device)
     } catch (e) {
@@ -20,7 +22,21 @@ class DeviceController {
   }
 
   async getAll(req, res){
-
+    const {brandId, typeId} = req.query
+    let devices;
+    if(!brandId && !typeId){
+      devices = await Device.findAll()
+    }
+    if(brandId && !typeId){
+      devices = await Device.findAll({where: {brandId}})
+    }
+    if(!brandId && typeId){
+      devices = await Device.findAll({where: {typeId}})
+    }
+    if(brandId && typeId){
+      devices = await Device.findAll({where: {brandId, typeId}})
+    }
+    return res.json(devices)
   }
 
   async getOne(req, res){
---
+++ server/controllers/resetId.js
@@ -0,0 +1,24 @@
+const { Client } = require('pg')
+require('dotenv').config();
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+})
+
+const resetId = (table) => {
+  client.connect();
+  client.query(`SELECT setval('${table}_id_seq', (SELECT MAX(id) FROM ${table}))`,
+  (err, res) => {
+    if(err){
+      console.error(err);
+    } else {console.log(res.rows[0])};
+    client.end();
+  });
+}
+
+
+module.exports = {resetId};
