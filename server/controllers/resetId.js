a48182f57afebbad2ef1bc15e897bbf59047cac3
Aleksei Chernyshev <chernyshev1998@gmail.com>
2023-12-11 19:47:02 +0500

    Auth  and login

--- server/controllers/resetId.js
+++ server/controllers/resetId.js
@@ -1,7 +1,7 @@
-const { Client } = require('pg')
+const { Pool } = require('pg')
 require('dotenv').config();
 
-const client = new Client({
+const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
@@ -10,10 +10,8 @@ const client = new Client({
 })
 
 const resetId = async (table) => {
-  await client.connect();
-  const res = await client.query(`SELECT setval('${table}_id_seq', (SELECT MAX(id) FROM ${table}))`);
+  const res = await pool.query(`SELECT setval('${table}_id_seq', (SELECT MAX(id) FROM ${table}))`);
   console.log(res.rows[0]);
-  client.end();
 }
 
 
